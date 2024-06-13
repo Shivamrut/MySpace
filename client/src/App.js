0077d6d99c37ea6bc88f9fb97da71d8947788b15
maxus_red <incineroaraditya@gmail.com>
2024-06-13 12:06:41 +0530

    basic frontend setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <><h1>Hello Mern</h1>
-      </>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={
+            <Home/>
+          }></Route>
+          <Route path='/about' element={
+            <About/>
+          }></Route>
+          
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <div className="container">
+                <h1>Welcome to INoteBook</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
